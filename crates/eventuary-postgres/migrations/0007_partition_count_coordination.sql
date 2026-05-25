17dbb773231e20a031d2a55a8f231193db8fa39c
Alan Boglioli <alan.boglioli@gmail.com>
2026-05-25 19:52:57 -0300

    chore(sql): remove flat migration directories

--- crates/eventuary-postgres/migrations/0007_partition_count_coordination.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE {partitions}
-ADD COLUMN IF NOT EXISTS partition_count INT NULL;
-
-CREATE INDEX IF NOT EXISTS idx_event_stream_partitions_group_stream_count
-ON {partitions} (consumer_group_id, stream_id, partition_count, partition_id);
